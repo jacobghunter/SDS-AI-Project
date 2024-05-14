9d638e6284dea8533487be9716988365f411c933
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2024-05-13 22:31:39 -0700

    changed port

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ with open('test.html', 'r') as f:
     html = f.read()
 
 # print(requests.get('http://localhost:5000/article'))
-print(requests.post('http://localhost:5000/upload_article/test', data=html))
+req = requests.get('http://167.99.111.147/article')
+req = requests.get('http://localhost:8000/article')
+print(req.json())
 # req = requests.post('http://localhost:5000/delete_article', json={"article": "Hello, World!"})
 
 # print(req)
