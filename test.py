7c3e4803b72cbbc227e1305385f930273b29ba45
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2024-05-15 17:50:12 -0700

    added gunicorn to requirements
    also added test.py to gitignore

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ with open('test.html', 'r') as f:
 
 # print(requests.get('http://localhost:5000/article'))
 req = requests.get('http://167.99.111.147/article')
-req = requests.get('http://localhost:8000/article')
-print(req.json())
+# req = requests.get('http://localhost:8000/article')
+print(req.text)
 # req = requests.post('http://localhost:5000/delete_article', json={"article": "Hello, World!"})
 
 # print(req)
